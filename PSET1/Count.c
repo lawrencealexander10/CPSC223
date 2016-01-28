83ef7450467dc3d176aa46e29841cf70b39d6f04
Alex Tenn <connection.l10@gmail.com>
2016-01-27 20:34:10 -0500

    after hacking specialwhitespace

--- PSET1/Count.c
+++ PSET1/Count.c
@@ -2,62 +2,78 @@
 #include <ctype.h>
 /* copy input to output; 1st version */
 int wordelse();
+int detectNewLine();
+int isValid();
 
-void main()
+int main()
 {
-int c;
+	int c;
 //holds previous character
-int previouschar;
+	int previouschar = 0;
 //holds line number
-long linenumber;
+	int linenumber = 0;
 //detects if there is whitespace between newlines and is 1 when there is a character
-int isThereACharacter;
+	int isThereACharacter= 0;
 //splice
-int splice;
+	int splice = 0;
 //else keyword
-int elseword;
+	int elseword= 0;
+//specialwhitespace
+	int specialwhitespace = 0;
 
-while (c = getchar() != EOF) {
+	while ((c = getchar()) != EOF) {
 //check if lines of code contain a character, if so not valid
- if(!isSpace() && isThereACharacter != 1){isThereACharacter = 1;} 
+		if(!isspace(c) &&  c != '{' && c !='}' && c != '(' && c != ')'  ){isThereACharacter = 1;} 
 
  //checks for line splices
-if (previouschar == '\\' && c == '\n'){
-	splice = 1;
-}
+		if (previouschar == '\\' && c == '\n'){
+			splice = 1;
+		}
 
+//check for specialwhitespace
+		if( c == '{' || c =='}' || c == '(' || c == ')'){
+			specialwhitespace = 1;
+			//printf("specialwhitespace");
+		}
 //check for "each"
-if(previouschar == ' ' && c == 'e'){
-	elseword = wordelse();
-	//in case wordelse reaches EOF
-	if(elseword == 100){
-		break;
-	}
-}
+	// 	if(previouschar == ' ' && c == 'e'){
+	// 		elseword = wordelse(c);
+	// //in case wordelse reaches EOF
+	// 		if(elseword == 100){
+	// 			break;
+	// 		}
+	// 	}
 
 //set previouschar
-previouschar = c;
+		previouschar = c;
 
 //if there is a new line
- if(detectNewLine(c)){
- 	++linenumber;
+		if(detectNewLine(c)){
+			++linenumber;
  	//check if previous line is valid
- 	if(isValid(isThereACharacter, splice, elseword)){
+			if(isValid(isThereACharacter, splice, specialwhitespace)){
  		//replace \n with  " //%d\n"
- 		printf(" //%d\n", linenumber, 'n');
- 		continue;
- 	}
- 	else{
- 		--linenumber;
- 	}
-
- 		//set variables to original state
+				printf(" //%d\n", linenumber);
+				 		//set variables to original state
 			isThereACharacter = 0;
 			splice = 0;
-			elseword = 0;
- }
-putchar(c);
-}
+			specialwhitespace = 0;
+				continue;
+			}
+			else{
+				--linenumber;
+				 		//set variables to original state
+			isThereACharacter = 0;
+			splice = 0;
+			specialwhitespace = 0;
+			}
+
+
+			
+			//elseword = 0;
+		}
+		putchar(c);
+	}
 }
 
 
@@ -67,59 +83,85 @@ putchar(c);
 //functions
 
 int detectNewLine( int c ){
-  if(c == '\n'){  
-  	return 1;
-  }
-  else{ 
-  	return 0;
-  }  
-}
-
-
-int isValid( int isThereACharacter, int splice, int elseword){
-	return (isThereACharacter == 1 && splice == 0 && elseword == 0) ? 1: 0;
-}
-
-int wordelse(){
-	for(int i = 0; i < 4; i++ ){
-		if(c = getchar() != EOF){
-			switch(i){
-				case '0':
-				if(c != 'l'){
-					return 0;
-				}
-				break;
-
-				case '1':
-				if(c != 's'){
-					return 0;
-				}
-				break;
-
-				case '2':
-				if(c != 'e'){
-					return 0;
-				}
-				break;
-
-				case ' 3':
-				if(c != ''){
-					return 0;
-				}
-				else{
-					return 1;
-				}
-				break;
-
-				default:
-				printf("Case is broken");
-				break;
-			}
-		}
-		else{
-			return 100;
-		}
+	if(c == '\n'){  
+		return 1;
 	}
+	else{ 
+		return 0;
+	}  
 }
 
-	
+
+int isValid( int isThereACharacter, int splice, int specialwhitespace){
+	int together = 0;
+	if((specialwhitespace == 1 && isThereACharacter == 1) || isThereACharacter == 1 ){
+		//printf("specialwhitespace"  );
+		together = 1;
+	}
+	// if (isThereACharacter)
+	// {
+	// 	printf("isThereACharacter"  );
+	// }
+	// 
+	// if( (isThereACharacter == 1 && specialwhitespace == 1) || specialwhitespace == 0) {
+	// 	together = 1;
+	// }
+	return (isThereACharacter == 1 && splice == 0 && together == 1 ) ? 1: 0;
+}
+
+// int wordelse(int pastc){
+// 	int c;
+//  putchar(pastc)
+// 	for(int i = 0; i < 4; i++ ){
+// 		if((c = getchar()) != EOF){
+// 			switch(i){
+// 				case '0':
+// 				if(c != 'l'){
+// 					return 0;
+// 				}
+// 				else{
+// 					printf("#2");
+// 				}
+// 				break;
+
+// 				case '1':
+// 				if(c != 's'){
+// 					return 0;
+// 				}
+// 				else{
+// 					printf("#2");
+// 				}
+// 				break;
+
+// 				case '2':
+// 				if(c != 'e'){
+// 					return 0;
+// 				}
+// 				else{
+// 					putchar(c);
+// 				}				
+// 				break;
+
+// 				case '3':
+// 				if(c != ' '){
+// 					return 0;
+// 				}
+// 				else{
+// 					return 1;
+// 				}
+// 				break;
+
+// 				// default:
+// 				// printf("Case is broken");
+// 				// return 99;
+// 				// break;
+// 			}
+// 		}
+// 		// else{
+// 		// 	return 100;
+// 		// }
+// 	}
+// 	return 0;
+// }
+
+
